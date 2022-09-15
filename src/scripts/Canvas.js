a99a3a2d3efb512d14b540d756d268f075734bc3
weston-bailey <wdbailey87@gmail.com>
2022-09-14 19:02:02 -0700

    refactors Canvas class to module

---
+++ src/scripts/Canvas.js
@@ -0,0 +1,34 @@
+const Mouse = require('./Mouse')
+
+module.exports = class Canvas {
+  #mouse
+  constructor({ parent }) {
+    this.parent = document.querySelector(parent)
+    
+    // set up the canvas
+    this.canvas = document.createElement('canvas')
+    this.canvas.classList.add('canvas')
+    this.canvas.setAttribute('height', getComputedStyle(this.canvas)['height']) 
+    this.canvas.setAttribute('width', getComputedStyle(this.canvas)['width'])
+    this.width = this.canvas.width
+    this.height = this.canvas.height
+    this.ctx = this.canvas.getContext('2d')
+    this.parent.appendChild(this.canvas)
+    this.#mouse = new Mouse({ element: this.canvas })
+  }
+
+  get mouse() {
+    return this.#mouse.position.copy()
+  }
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.width, this.height)
+  }
+
+  drawPath(cb) {
+    const { ctx } = this
+    ctx.beginPath()
+    cb(ctx)
+    ctx.stroke()
+  }
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,41 +1,8 @@
 require('../styles/index.css')
 const Vector = require('./Vector')
-const Mouse = require('./Mouse')
+const Canvas = require('./Canvas')
 
 document.addEventListener('DOMContentLoaded', main)
-
-class Canvas {
-  #mouse
-  constructor({ parent }) {
-    this.parent = document.querySelector(parent)
-    
-    // set up the canvas
-    this.canvas = document.createElement('canvas')
-    this.canvas.classList.add('canvas')
-    this.canvas.setAttribute('height', getComputedStyle(this.canvas)['height']) 
-    this.canvas.setAttribute('width', getComputedStyle(this.canvas)['width'])
-    this.width = this.canvas.width
-    this.height = this.canvas.height
-    this.ctx = this.canvas.getContext('2d')
-    this.parent.appendChild(this.canvas)
-    this.#mouse = new Mouse({ element: this.canvas })
-  }
-
-  get mouse() {
-    return this.#mouse.position.copy()
-  }
-
-  clear() {
-    this.ctx.clearRect(0, 0, this.width, this.height)
-  }
-
-  drawPath(cb) {
-    const { ctx } = this
-    ctx.beginPath()
-    cb(ctx)
-    ctx.stroke()
-  }
-}
 
 function main() {
   const vSubCanvas = new Canvas({ parent: '#vector-sub' })
