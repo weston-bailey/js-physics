08c21b71f9ad28dc7767f18645d8649cd28363fd
weston-bailey <wdbailey87@gmail.com>
2022-09-17 16:35:52 -0700

    adds deploy script

--- src/scripts/lib/Physics/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    RectangleAxisAligned: require('./RectangleAxisAligned'),
-    Vector: require('./Vector'),
-    Body: require('./Body'),
-    Gravity: require('./Gravity')
-}
--- src/scripts/lib/Render/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    Canvas: require('./Canvas')
-}
