39f0031e5c1c31845b8d789b2bcf688cab8a68ca
weston-bailey <wdbailey87@gmail.com>
2022-12-11 22:08:05 -0800

    removing build

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ git commit -m 'pushing ./dist to be deployed on gh-pages'
 git push --delete origin gh-pages
 git subtree push --prefix dist origin gh-pages
 rm -rf ./dist
+git add .
 git commit -m 'removing build'
